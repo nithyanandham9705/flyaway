583493b0cbe872af3caeba3c996560e9ff3ddfdf
nithyanandham9705 <90197054+nithyanandham9705@users.noreply.github.com>
2023-03-14 22:13:19 +0530

    Update AddFareDetailsSubmit.java

--- src/com/flyaway/controller/AddFareDetailsSubmit.java
+++ src/com/flyaway/controller/AddFareDetailsSubmit.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class AddFareDetailSubmit
+ * Servlet implementation class AddFarEDetailSubmit
  */
 @WebServlet("/addfaredetailssubmit")
 public class AddFareDetailsSubmit extends HttpServlet {
